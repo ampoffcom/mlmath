ee453eda8a5c0a6f46db0b05cd23f388739c780b
Steffen Fritz <steffenfritz@amp-off.com>
2015-08-28 10:42:01 +0200

    added Bubblesort

--- mlmath.go
+++ mlmath.go
@@ -2,12 +2,39 @@ package mlmath
 
 import (
 	"errors"
+	"fmt"
 	"math"
 )
 
 type Klabel struct {
 	Values []float64
 	Label  string
+}
+
+// Bubblesort for Klabel and repetitive sorting for small k
+
+func KlabelBsort(a []Klabel) {
+	a_len := len(a)
+	a_temp := make([]Klabel, a_len)
+
+	copy(a_temp, a)
+
+	for i, _ := range a {
+		if i+1 != a_len {
+			if a[i+1].Values[0] < a[i].Values[0] {
+				a[i+1].Values[0], a[i].Values[0] = a[i].Values[0], a[i+1].Values[0]
+			}
+			copy(a_temp, a)
+
+			for i, _ := range a_temp {
+				if a_temp[i].Values[0] != a[i].Values[0] {
+					KlabelBsort(a)
+				}
+			}
+		}
+	}
+
+	fmt.Print(a)
 }
 
 func Check_length(p []float64, q []float64) error {
@@ -44,14 +71,17 @@ func Manhatten(p []float64, q []float64) float64 {
 }
 
 func KNN(in []float64, tdata []Klabel, k int) []Klabel {
-	k_res := make([]Klabel, k)
+	k_res := make([]Klabel, 0)
 	var temp_res Klabel
 
 	for _, tvalues := range tdata {
 		temp_val := Manhatten(in, tvalues.Values)
 		temp_res.Values = []float64{temp_val}
 		temp_res.Label = tvalues.Label
-		k_res = append(k_res, temp_res)
+		if len(k_res) < k {
+			k_res = append(k_res, temp_res)
+		} else {
+		}
 	}
 
 	return k_res
